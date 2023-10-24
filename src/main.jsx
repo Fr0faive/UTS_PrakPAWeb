9fff3ae23eb5e99182f9052d0b53fd9b688e9498
fr0faive <faikar.moch.tajudin@gmail.com>
2023-10-24 23:31:08 +0700

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import LandingPage from './pages/landingPage'
+import LandingPage from './pages/LandingPage'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
